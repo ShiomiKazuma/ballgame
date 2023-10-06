e1609990b1d35881ba42dc602d0085e73d0b97f5
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-10-06 19:02:26 +0900

    [fix]ゲームオーバーの処理を修正

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
@@ -8,14 +9,25 @@ public class BallController : MonoBehaviour
     public int SelfNumber { get; set; } = 0;
     [SerializeField] int _level = 0;
     [SerializeField] int _levelMax = 0;
+    bool _gameOverJudge = false;
+    [SerializeField] GameObject _text;
     void Awake()
     {
         _totalNumber++;
         SelfNumber = _totalNumber;
     }
+
+    private void Update()
+    {
+        
+    }
     //アイテム通しが当たった時の処理
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(!(_gameOverJudge))
+        {
+            _gameOverJudge = true;
+        }
         //ItemManager.Instance.BallObject[0];
         if(collision.transform.TryGetComponent(out BallController ball) && ball.SelfNumber < SelfNumber)
         {
@@ -29,6 +41,19 @@ public class BallController : MonoBehaviour
                 Destroy(collision.gameObject);
                 Instantiate(ItemManager.Instance.BallObject[_level + 1], new Vector3(pos.x, pos.y, 0), Quaternion.identity);
             }        
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(_gameOverJudge && collision.gameObject.tag == "GameOver")
+        {
+            // ゲームオーバー時の処理
+            //Text text = _text.GetComponent<Text>();
+            //text.text = "Game Over";
+            //_text.SetActive(true);
+            //Time.timeScale = 0;
+            Debug.Log("ゲームオーバー");
         }
     }
 }
